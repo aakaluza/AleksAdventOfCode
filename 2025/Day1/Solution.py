9afcc2dc867467015b56283eb380f9af5be0b5a7
aakaluza <aakaluza83@gmail.com>
2025-12-09 22:00:19 -0500

    Part 2 Completed

--- 2025/Day1/Solution.py
+++ 2025/Day1/Solution.py
@@ -10,27 +10,25 @@ def rotateLock(part: int, distances: List[int]):
     pointer = 50
 
     for distance in distances:
-        rotatedPointer = pointer + distance
-        pointer = (rotatedPointer) % 100
+        if part == 2:
+            if distance > 0: password += (pointer + distance) // 100
+            else: password += ((pointer * -1) % 100 + abs(distance)) // 100
+        
+        pointer = (pointer + distance) % 100
 
         if part == 1 and pointer == 0: password += 1
-
-        if part == 2 and (rotatedPointer < 0 or rotatedPointer > 99):
-            # Issue here is negatives
-            # Lets do some math man cmon
-            password += abs(rotatedPointer // 100)
     
     return password
 
 def main():
     distances = []
-    with open("/Users/alekkaluza/Projects/AleksAdventOfCode/2025/Day1/puzzleInput.txt", "r") as input:
+    with open("puzzleInput.txt", "r") as input:
         for line in input:
             direction = -1 if line[0] == 'L' else 1
             distance = int(line[1:]) * direction
             distances.append(distance)
     
-    with open("/Users/alekkaluza/Projects/AleksAdventOfCode/2025/Day1/solution.txt", "w") as solution:
+    with open("solution.txt", "w") as solution:
         for i in range(1, 3, 1):
             solution.write(f'Alek\'s Part {i} Solution: {rotateLock(i, distances)}\n')
 
